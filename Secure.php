74f8bb0570a1f50f8510c1554fff9117e988bfb0
Areyi <fariz.studionexus@gmail.com>
2016-01-03 20:02:23 +0800

    Create Secure.php

---
+++ Secure.php
@@ -0,0 +1,29 @@
+<?php 
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Contracts\Routing\Middleware;
+/**
+ * Secure
+ * Redirects any non-secure requests to their secure counterparts.
+ * 
+ * @param request The request object.
+ * @param $next The next closure.
+ * @return redirects to the secure counterpart of the requested uri.
+*/
+class Secure
+{
+	
+
+    public function handle($request, Closure $next)
+    {
+           if(!$request->secure() || substr($request->header('host'), 0, 4)  != 'www.'){
+               $request->setTrustedProxies( [ $request->getClientIp() ] ); 
+               $request->headers->set('host', 'www.studionexus.co');
+               return redirect()->secure($request->getRequestUri());
+           }
+
+           return $next($request); 
+
+    }
+	
+}
